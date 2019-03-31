f139dee4edb9fb84d58c0792ba133d53135b3a8b
ibaris <i.baris@outlook.de>
2019-03-31 16:32:13 +0200

    feat(rspy.auxiliary.types): Add angle units and checks

--- rspy/auxiliary/types.py
+++ rspy/auxiliary/types.py
@@ -5,7 +5,8 @@ Created on 31.03.2019 by Ismail Baris
 from __future__ import division
 import numpy as np
 
-__all__ = ['valid_dtype']
+__all__ = ['valid_dtype', 'valid_angle_deg', 'valid_angle_rad', 'check_angle_unit']
+
 __DTYPES__ = [np.short, np.ushort, np.intc, np.uintc, np.int_, np.uint, np.longlong,
               np.ulonglong, np.half, np.float, np.float16, np.single, np.double, np.longdouble, np.csingle,
               np.cdouble,
@@ -16,6 +17,28 @@ __DTYPES__ = [np.short, np.ushort, np.intc, np.uintc, np.int_, np.uint, np.longl
 
 __UNIT_RAD__ = ['RAD', 'rad', 'radian', 'radians']
 __UNIT_DEG__ = ['DEG', 'deg', 'degree', 'degrees']
+
+
+def valid_angle_deg(unit):
+    if unit in __UNIT_DEG__:
+        return True
+
+    return False
+
+
+def valid_angle_rad(unit):
+    if unit in __UNIT_RAD__:
+        return True
+
+    return False
+
+
+def check_angle_unit(unit):
+    if valid_angle_deg(unit) or valid_angle_rad(unit):
+        pass
+    else:
+        raise ValueError("Unit `{0}` not understood. Supported angle units "
+                         "are: {1} and {2}".format(str(unit), __UNIT_RAD__, __UNIT_DEG__))
 
 
 def valid_dtype(dtype):
