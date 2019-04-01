86eebdfe34b881c2792da17d0a5f3c70ee8cbf86
ibaris <i.baris@outlook.de>
2019-04-01 16:43:22 +0200

    chore(rspy.setup):

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 
-from setuptools import find_packages
 import numpy
+from setuptools import find_packages
 
 
 def get_version():
@@ -35,8 +35,7 @@ def get_version():
 
 
 def get_packages():
-    find_packages(exclude=['docs']),
-    return find_packages()
+    return find_packages(exclude=['docs']),
 
 
 # with open('requirements.txt') as f:
