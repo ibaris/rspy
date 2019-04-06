967d4654c973f3fab2defe3ed5e98c383ae0593c
ibaris <i.baris@outlook.de>
2019-04-06 17:41:44 +0200

    feat(rspy.constants): Add c to constants

--- rspy/constants/constants.py
+++ rspy/constants/constants.py
@@ -4,6 +4,7 @@ Created on  by Ismail Baris
 """
 from __future__ import division
 
-__all__ = ['pi']
+__all__ = ['pi', 'c']
 
 pi = 3.1415926535897932384626433832795028841971
+c = 299792458.0  # Speed of light in m / s
