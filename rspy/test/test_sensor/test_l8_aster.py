35ca27690ad1e58079d32ede3ab108c55f27b861
ibaris <i.baris@outlook.de>
2019-04-07 13:53:22 +0200

    chore(rspy): Changes due to CodeFactor

--- rspy/test/test_sensor/test_l8_aster.py
+++ rspy/test/test_sensor/test_l8_aster.py
@@ -32,7 +32,7 @@ class TestAllDicts:
             a_item = item(a=a)
 
             with pytest.raises(AttributeError):
-                a_item.b
+                p = a_item.b
 
     def test_dicts_dir(self):
         for item in dicts:
